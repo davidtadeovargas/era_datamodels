03b2c6921be2e4d48ed35cfe37a588aa7c2de024
davidtadeovargas <coritocorito@hotmail.com>
2020-06-13 23:47:43 -0600

    Varios ajustes
    -Que las listas de precios funcionen en el catálogo de productos
    -Utilidad para formatear a y desde moneda

---
+++ src/main/java/com/era/datamodels/LPrecsDatamodel.java
@@ -0,0 +1,288 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.era.datamodels;
+
+/**
+ *
+ * @author PC
+ */
+public class LPrecsDatamodel {
+    
+    private String priceList1;
+    private String priceList2;
+    private String priceList3;
+    private String priceList4;
+    private String priceList5;
+    private String priceList6;
+    private String priceList7;
+    private String priceList8;
+    private String priceList9;
+    private String priceList10;
+
+    private String utility1;
+    private String utility2;
+    private String utility3;
+    private String utility4;
+    private String utility5;
+    private String utility6;
+    private String utility7;
+    private String utility8;
+    private String utility9;
+    private String utility10;
+
+    private String utilSales1;
+    private String utilSales2;
+    private String utilSales3;
+    private String utilSales4;
+    private String utilSales5;
+    private String utilSales6;
+    private String utilSales7;
+    private String utilSales8;
+    private String utilSales9;
+    private String utilSales10;
+
+    public String getPriceList1() {
+        return priceList1;
+    }
+
+    public void setPriceList1(String priceList1) {
+        this.priceList1 = priceList1;
+    }
+
+    public String getPriceList2() {
+        return priceList2;
+    }
+
+    public void setPriceList2(String priceList2) {
+        this.priceList2 = priceList2;
+    }
+
+    public String getPriceList3() {
+        return priceList3;
+    }
+
+    public void setPriceList3(String priceList3) {
+        this.priceList3 = priceList3;
+    }
+
+    public String getPriceList4() {
+        return priceList4;
+    }
+
+    public void setPriceList4(String priceList4) {
+        this.priceList4 = priceList4;
+    }
+
+    public String getPriceList5() {
+        return priceList5;
+    }
+
+    public void setPriceList5(String priceList5) {
+        this.priceList5 = priceList5;
+    }
+
+    public String getPriceList6() {
+        return priceList6;
+    }
+
+    public void setPriceList6(String priceList6) {
+        this.priceList6 = priceList6;
+    }
+
+    public String getPriceList7() {
+        return priceList7;
+    }
+
+    public void setPriceList7(String priceList7) {
+        this.priceList7 = priceList7;
+    }
+
+    public String getPriceList8() {
+        return priceList8;
+    }
+
+    public void setPriceList8(String priceList8) {
+        this.priceList8 = priceList8;
+    }
+
+    public String getPriceList9() {
+        return priceList9;
+    }
+
+    public void setPriceList9(String priceList9) {
+        this.priceList9 = priceList9;
+    }
+
+    public String getPriceList10() {
+        return priceList10;
+    }
+
+    public void setPriceList10(String priceList10) {
+        this.priceList10 = priceList10;
+    }
+
+    public String getUtility1() {
+        return utility1;
+    }
+
+    public void setUtility1(String utility1) {
+        this.utility1 = utility1;
+    }
+
+    public String getUtility2() {
+        return utility2;
+    }
+
+    public void setUtility2(String utility2) {
+        this.utility2 = utility2;
+    }
+
+    public String getUtility3() {
+        return utility3;
+    }
+
+    public void setUtility3(String utility3) {
+        this.utility3 = utility3;
+    }
+
+    public String getUtility4() {
+        return utility4;
+    }
+
+    public void setUtility4(String utility4) {
+        this.utility4 = utility4;
+    }
+
+    public String getUtility5() {
+        return utility5;
+    }
+
+    public void setUtility5(String utility5) {
+        this.utility5 = utility5;
+    }
+
+    public String getUtility6() {
+        return utility6;
+    }
+
+    public void setUtility6(String utility6) {
+        this.utility6 = utility6;
+    }
+
+    public String getUtility7() {
+        return utility7;
+    }
+
+    public void setUtility7(String utility7) {
+        this.utility7 = utility7;
+    }
+
+    public String getUtility8() {
+        return utility8;
+    }
+
+    public void setUtility8(String utility8) {
+        this.utility8 = utility8;
+    }
+
+    public String getUtility9() {
+        return utility9;
+    }
+
+    public void setUtility9(String utility9) {
+        this.utility9 = utility9;
+    }
+
+    public String getUtility10() {
+        return utility10;
+    }
+
+    public void setUtility10(String utility10) {
+        this.utility10 = utility10;
+    }
+
+    public String getUtilSales1() {
+        return utilSales1;
+    }
+
+    public void setUtilSales1(String utilSales1) {
+        this.utilSales1 = utilSales1;
+    }
+
+    public String getUtilSales2() {
+        return utilSales2;
+    }
+
+    public void setUtilSales2(String utilSales2) {
+        this.utilSales2 = utilSales2;
+    }
+
+    public String getUtilSales3() {
+        return utilSales3;
+    }
+
+    public void setUtilSales3(String utilSales3) {
+        this.utilSales3 = utilSales3;
+    }
+
+    public String getUtilSales4() {
+        return utilSales4;
+    }
+
+    public void setUtilSales4(String utilSales4) {
+        this.utilSales4 = utilSales4;
+    }
+
+    public String getUtilSales5() {
+        return utilSales5;
+    }
+
+    public void setUtilSales5(String utilSales5) {
+        this.utilSales5 = utilSales5;
+    }
+
+    public String getUtilSales6() {
+        return utilSales6;
+    }
+
+    public void setUtilSales6(String utilSales6) {
+        this.utilSales6 = utilSales6;
+    }
+
+    public String getUtilSales7() {
+        return utilSales7;
+    }
+
+    public void setUtilSales7(String utilSales7) {
+        this.utilSales7 = utilSales7;
+    }
+
+    public String getUtilSales8() {
+        return utilSales8;
+    }
+
+    public void setUtilSales8(String utilSales8) {
+        this.utilSales8 = utilSales8;
+    }
+
+    public String getUtilSales9() {
+        return utilSales9;
+    }
+
+    public void setUtilSales9(String utilSales9) {
+        this.utilSales9 = utilSales9;
+    }
+
+    public String getUtilSales10() {
+        return utilSales10;
+    }
+
+    public void setUtilSales10(String utilSales10) {
+        this.utilSales10 = utilSales10;
+    }
+    
+    
+}
